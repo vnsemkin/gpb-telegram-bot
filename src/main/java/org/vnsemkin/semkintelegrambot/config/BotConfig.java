03441713dcc9832d4aef01b7c2ec7bc0507bbc71
vnsemkin <vnsemkin@gmail.com>
2024-05-14 17:12:21 +0300

    Задание 3. Создан "скелет" бота

--- src/main/java/org/vnsemkin/semkintelegrambot/config/BotConfig.java
+++ src/main/java/org/vnsemkin/semkintelegrambot/config/BotConfig.java
@@ -3,19 +3,14 @@ package org.vnsemkin.semkintelegrambot.config;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import org.vnsemkin.semkintelegrambot.component.TelegramBot;
-import org.vnsemkin.semkintelegrambot.constant.BotConstant;
 
 @Getter
 @Configuration
-@PropertySource(BotConstant.PROPERTY_SOURCE)
-@ComponentScan(basePackages = "org.vnsemkin.semkintelegrambot")
 public class BotConfig {
     @Value("${telegram.gpb-bot.name}")
     private String botName;
