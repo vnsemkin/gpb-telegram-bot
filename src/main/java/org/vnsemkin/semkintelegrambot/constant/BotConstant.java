1837724107c4b26b7b58700343edc3c6216319f4
vnsemkin <vnsemkin@gmail.com>
2024-05-14 17:14:23 +0300

    Задание 3. Создан "скелет" бота

--- src/main/java/org/vnsemkin/semkintelegrambot/constant/BotConstant.java
+++ src/main/java/org/vnsemkin/semkintelegrambot/constant/BotConstant.java
@@ -7,5 +7,4 @@ public class BotConstant {
     public static final String PONG_ANSWER = "pong";
     public static final String HTML_MARKUP = "HTML";
     public static final String NOT_IMPLEMENTED = "Функция в разработке";
-    public static final String PROPERTY_SOURCE = "/application.yml";
 }
