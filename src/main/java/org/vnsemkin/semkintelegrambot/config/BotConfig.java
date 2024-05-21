9829a7a8402ac9f116fc1afcd08ab5353381b083
vnsemkin <vnsemkin@gmail.com>
2024-05-21 15:41:59 +0300

    Разрешены конфликты с trunk 2.

--- src/main/java/org/vnsemkin/semkintelegrambot/config/BotConfig.java
+++ src/main/java/org/vnsemkin/semkintelegrambot/config/BotConfig.java
@@ -2,7 +2,12 @@ package org.vnsemkin.semkintelegrambot.config;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.vnsemkin.semkintelegrambot.component.TelegramBot;
 
 @Getter
 @Configuration
@@ -11,4 +16,16 @@ public class BotConfig {
     private String botName;
     @Value("${telegram.gpb-bot.token}")
     private String token;
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(TelegramBot telegramBot) {
+        TelegramBotsApi telegramBotsApi;
+        try {
+            telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(telegramBot);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+        return telegramBotsApi;
+    }
 }
--- src/main/java/org/vnsemkin/semkintelegrambot/constant/BotConstant.java
+++
@@ -1,8 +0,0 @@
-package org.vnsemkin.semkintelegrambot.constant;
-
-public final class BotConstant {
-    public static final String COMMAND_DELIMITER = "/";
-    public static final String PONG_ANSWER = "pong";
-    public static final String HTML_MARKUP = "HTML";
-    public static final String NOT_IMPLEMENTED = "Функция в разработке";
-}
