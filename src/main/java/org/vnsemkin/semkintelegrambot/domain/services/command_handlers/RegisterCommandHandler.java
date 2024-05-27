10140d04bee047bd0ca1f3899ab8dac8e1938cf3
vnsemkin <vnsemkin@gmail.com>
2024-05-27 14:32:22 +0300

    Added some changes.

--- src/main/java/org/vnsemkin/semkintelegrambot/domain/services/command_handlers/RegisterCommandHandler.java
+++ src/main/java/org/vnsemkin/semkintelegrambot/domain/services/command_handlers/RegisterCommandHandler.java
@@ -1,23 +1,27 @@
 package org.vnsemkin.semkintelegrambot.domain.services.command_handlers;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.vnsemkin.semkintelegrambot.domain.constants.BotCommand;
-import org.vnsemkin.semkintelegrambot.domain.services.common.Sender;
+import org.vnsemkin.semkintelegrambot.domain.constants.CommandToServiceMap;
+import org.vnsemkin.semkintelegrambot.domain.services.reply_handlers.RegistrationService;
 
 @Service
 @RequiredArgsConstructor
 public class RegisterCommandHandler implements CommandHandler {
-    private final Sender sender;
+    private final RegistrationService registrationService;
 
     @Override
     public void handle(long chatId) {
-
+        registrationService.startPickUpInformation(chatId);
     }
 
     @Override
     public String getCommand() {
-        return BotCommand.REGISTER.command;
+        return CommandToServiceMap.REGISTER.command;
+    }
+
+    @Override
+    public String getServiceName() {
+        return CommandToServiceMap.REGISTER.service;
     }
 }
